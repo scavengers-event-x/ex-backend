7713bcf75357716298bfa121e0f1f2cf08b61b05
Prazwal Malakar <its.prazwal@gmail.com>
2022-12-31 17:32:24 +0545

    fixes on user address

--- src/models/user/userModel/user.mapping.ts
+++ src/models/user/userModel/user.mapping.ts
@@ -5,13 +5,10 @@ export const userProfileMapping = (data2: FieldTypeUserProfile): { profile: Fiel
   if (data2) {
     if (data2.fullName) (data1 as FieldTypeUserProfile).fullName = data2.fullName
     if (data2.gender) (data1 as FieldTypeUserProfile).gender = data2.gender
-    if (data2.address) {
-      const add = (data2 as FieldTypeUserProfile).address
-      if (add.tole) data1.address = { ...data1.address, tole: add.tole }
-      if (add.city) data1.address = { ...data1.address, city: add.city }
-      if (add.ward) data1.address = { ...data1.address, ward: add.ward }
-      if (add.province) data1.address = { ...data1.address, province: add.province }
-    }
+    if (data2['address.tole']) data1.address = { ...data1.address, tole: data2['address.tole'] }
+    if (data2['address.city']) data1.address = { ...data1.address, city: data2['address.city'] }
+    if (data2['address.ward']) data1.address = { ...data1.address, ward: data2['address.ward'] }
+    if (data2['address.province']) data1.address = { ...data1.address, province: data2['address.province'] }
     if (data2.phone) (data1 as FieldTypeUserProfile).phone = data2.phone
     if (data2.citizenship) (data1 as FieldTypeUserProfile).citizenship = data2.citizenship
     if (data2.pan) (data1 as FieldTypeUserProfile).pan = data2.pan
