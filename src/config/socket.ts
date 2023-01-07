7f431e82c2c237c0eb905b457eb5783dadba999b
Prazwal Malakar <its.prazwal@gmail.com>
2023-01-08 03:02:21 +0545

    Fixes on socket message

--- src/config/socket.ts
+++ src/config/socket.ts
@@ -16,7 +16,7 @@ io.on('connection', (socket) => {
   socket.on('send-msg', (data) => {
     const sendUserSocket = global.onlineUsers.get(data.to)
     if (sendUserSocket) {
-      socket.to(sendUserSocket).emit('msg-receive', data.msg)
+      socket.to(sendUserSocket).emit('msg-receive', data.message)
     }
   })
 })
